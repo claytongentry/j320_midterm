4ca30774b62243b4c19ba7b4d6695bb5ebd2e531
claytongentry <claytongentry2017@u.northwestern.edu>
2016-04-26 19:12:55 -0500

    file structure organized

---
+++ app.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var app = express();
+var port = 3000 || process.env.PORT;
+
+app.use(express.static(__dirname + '/public'));
+
+app.listen(port, function() {
+  console.log("Cookin' on port " + port);
+});
